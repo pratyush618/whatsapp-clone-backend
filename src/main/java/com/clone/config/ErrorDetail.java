f21c10314df89c4158273b4da0eca232f0fe726c
Pratyush Sharma <pratyush.sharma618@gmail.com>
2023-11-29 12:55:20 +0530

    Modified: Exception Handler Constructor

--- src/main/java/com/clone/config/ErrorDetail.java
+++ src/main/java/com/clone/config/ErrorDetail.java
@@ -2,12 +2,17 @@ package com.clone.config;
 
 import java.time.LocalDateTime;
 
+@SuppressWarnings("unused")
 public class ErrorDetail {
 
     private String message;
     private String error;
     private LocalDateTime timestamp;
 
-    public ErrorDetail(String error, String message, LocalDateTime LocalDateTime now)
+    public ErrorDetail(String error, String message, LocalDateTime timestamp) {
+        this.error = error;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
 
 }
