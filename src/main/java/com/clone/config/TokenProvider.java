6ebe1c1a7cb9bb56e2b2d0831fc600bd0eef51c7
Pratyush Sharma <pratyush.sharma618@gmail.com>
2023-11-29 12:14:17 +0530

    DI of user email

--- src/main/java/com/clone/config/TokenProvider.java
+++ src/main/java/com/clone/config/TokenProvider.java
@@ -1,13 +1,19 @@
 package com.clone.config;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Service
+@SuppressWarnings("unused")
 public class TokenProvider {
+
+    SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
 
     String generateToken(Authentication authentication) {
         return String.valueOf(Jwts.builder()
@@ -17,6 +23,13 @@ public class TokenProvider {
                 .claim("email", authentication.getName())
                 .signWith(null)
                 .compact());
+    }
+
+    String getEmailFromToken(String token) {
+        token = token.substring(7);
+        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+
+        return String.valueOf(claims.get("username"));
     }
 
 }
