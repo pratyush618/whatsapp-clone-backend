d519e66f1278792a314e121107e5f9bb9ad311bb
Pratyush Sharma <pratyush.sharma618@gmail.com>
2023-12-06 15:11:31 +0530

    ~

--- src/main/java/com/clone/model/Notification.java
+++ src/main/java/com/clone/model/Notification.java
@@ -1,4 +1,25 @@
 package com.clone.model;
 
+import lombok.Data;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
 public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String content;
+
+    @Column(name = "local_date_time")
+    private LocalDateTime localDateTime;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
 }
--- src/main/java/com/clone/request/RenameGroupRequest.java
+++ src/main/java/com/clone/request/RenameGroupRequest.java
@@ -1,4 +1,11 @@
 package com.clone.request;
 
+import lombok.Data;
+
+@Data
 public class RenameGroupRequest {
+
+    private Integer chatId;
+    private String groupName;
+
 }
